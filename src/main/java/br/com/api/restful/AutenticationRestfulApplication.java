4eb0240c2f697e6e7bd08e87a730228075ba8a52
Halyson <halysonrp@gmail.com>
2019-03-05 15:57:13 -0300

    import project gitHub

---
+++ src/main/java/br/com/api/restful/AutenticationRestfulApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.restful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutenticationRestfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutenticationRestfulApplication.class, args);
+	}
+
+}
